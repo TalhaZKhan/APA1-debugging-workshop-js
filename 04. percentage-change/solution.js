9333f3e11aef0aaf4899b4fe36e11a04a0507555
Talha Khan <81024827+TalhaZKhan@users.noreply.github.com>
2024-12-09 12:22:58 +0000

    Solved 4

--- 04. percentage-change/solution.js
+++ 04. percentage-change/solution.js
@@ -1,8 +1,10 @@
 function calculatePercentageChange(original, newAmount) {
-  const difference = Math.abs(newAmount - original);
-  const average = (original - newAmount) / 2;
-  const percentageDifference = (average / difference) * 100;
-  return percentageDifference.toFixed(2);
+  if(original == newAmount){
+    return 0
+  }
+  const difference = newAmount - original;
+  const percentageDifference = (difference/original) * 100;
+  return Number(percentageDifference.toFixed(0));
 }
 
 module.exports = { calculatePercentageChange };
